f3a36a89e15195ccc8cb13c7fb9d692433a8bf39
Bharath From Sravya's Computer <bharry29@gmail.com>
2019-01-31 21:49:14 -0800

    updated

--- calculator/src/keyContainer.js
+++ calculator/src/keyContainer.js
@@ -5,14 +5,14 @@ import './keyContainer.css';
 function KeyContainer(props) {
     let keys = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '.']
     return (
-        <div className="container">
+        <div className="container keypad-container">
             <div className="row number-container col-md-8">
 
 
                 {keys.map((x) => <div className="col-lg-4 key-btn-container"><button className="btn btn-info key-number">{x}</button></div>)}
             </div>
 
-            <div class="row operation-container">
+            <div class="row operation-container col-md-4">
 
             </div>
         </div>
